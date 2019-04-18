91bb0180658ce3bc5ddbcff92198544e7f3cc726
Shashwat Mishra <shashwat.x@gmail.com>
2019-04-18 10:29:55 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Introduction
+
+Crawls Playstore reviews using Selenium.
+
+## Required packages
+
+Package | Version | Description
+---- | ----|-------
+[coloredlogs](https://pypi.org/project/coloredlogs/)|7.3| Logging
+[selenium](https://pypi.org/project/selenium/) |3.141.0| Web Driver 
+[bs4](https://pypi.org/project/beautifulsoup4/) |4.7.1| HTML Parser 
+[click](https://pypi.org/project/click/) |7.0| Command line args
+
+[time](https://docs.python.org/2/library/time.html) |n/a| Time related functions
+[csv](https://docs.python.org/2/library/csv.html) |1.0| Charting
+[os](https://docs.python.org/2/library/os.html) |n/a| Server-side web framework
+[logging](https://docs.python.org/2/library/logging.html) | 0.5.1.2 | Logging
+[re](https://expressjs.com/) |2.2.1| Regex operations
+
+## Show Usage 
+```
+python crawler.py --help
+```
+
+### Example
+```
+python crawler.py -i ./input.txt -o data/ -d ./assets/chromedriver_linux64
+```
---
+++ crawler.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+
+import coloredlogs
+coloredlogs.install()
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+from pdb import set_trace as bp ##for testing
+import re
+import time
+import csv
+import os
+import logging
+logging.basicConfig(filename='execution.log',level=logging.INFO)
+logger = logging.getLogger("main")
+import click
+
+# prefix for output files
+namePrefix='app'
+
+# missing suffix in the array
+urlSuffix='&showAllReviews=true'
+
+# number of times you need to "scroll-to-end" to see "Show More" button
+numBatches=4
+
+# batch report parameter
+report_pool_size=5
+
+# skip title if enough reviews found.
+reviewLimit=12000
+
+
+def obtainOutputFileName(appCounter,s):
+    s=re.sub(r"[^\w\s]",'',s)
+    s=re.sub(r"[\s+]",'-',s)
+    outputFileName=namePrefix+'_'+str(appCounter)+'_'+s
+    return outputFileName
+
+def readInputFile(pathToInputFile):
+    with open(pathToInputFile) as f:
+        text = f.read()
+        listOfURLS=text.splitlines()
+    return listOfURLS
+
+@click.command()
+@click.option('--input', '-i', 'input_', required=True, type=click.Path(exists=True))
+@click.option('--output', '-o', required=True, type=click.Path(exists=True))
+@click.option('--driver', '-d', 'driver_', required=True, type=click.Path(exists=True))
+def run(input_,output,driver_):
+    """Simple spider to get Playstore reviews."""
+
+    logger.info('Input: %s.',os.path.expanduser(input_))
+    listOfURLS=readInputFile(input_)
+    numTitles=len(listOfURLS)
+    logger.info('Number of Titles: %d',numTitles)
+
+    logger.info('Web Driver: %s.',os.path.expanduser(driver_))
+    chrome_options = Options()
+    driver = webdriver.Chrome(executable_path=os.path.realpath(driver_), chrome_options=chrome_options)
+
+    appCounter=-1
+    for lt in listOfURLS:
+        appCounter=appCounter+1
+
+        link=lt+urlSuffix
+        driver.get(link)
+        title = driver.find_element_by_xpath('//*[@id="fcxH9b"]/div[4]/c-wiz/div/div[3]/meta[2]').get_attribute('content')
+
+        outputFile=os.path.join(output,obtainOutputFileName(appCounter,title))
+
+        logger.warn('Title: %s.',title)
+        logger.warn('URL: %s', link)
+        logger.warn('Output: %s', outputFile)
+
+        flag=0
+        iterx=0
+        while 1:
+            logger.info('App %02d/%02d...Iteration #%d',appCounter,(numTitles-1),iterx)
+
+            if iterx%report_pool_size == 0 and iterx!=0:
+                numReviews=len(driver.find_elements_by_xpath("//*[@jsname='fk8dgd']//div[@class='d15Mdf bAhLNe']"))
+                logger.info('We are on iteration %d, \t number of reviews: %d',iterx,numReviews)
+                if numReviews>reviewLimit:
+                    logger.warn('Enough reviews gathered. %d',numReviews)
+                    break
+
+            # "Show More" button appears only after 4 complete scroll-to-bottom operations
+            for i in range(1,numBatches+1):
+                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                logger.info('Scroll %d is done.', i)
+                time.sleep(2)
+
+            # "Show More" should now appear
+            try:
+                loadMore=driver.find_element_by_xpath("//*[contains(@class,'U26fgb O0WRkf oG5Srb C0oVfc n9lfJ')]").click()
+            except Exception , e:
+                logger.error('Encountered exception: %s',type(e))
+                flag=flag+1
+                logger.debug('Flag is now %d.',flag)
+                if flag >= 2:
+                    logger.warn('I suppose I have reached the end.')
+                    logger.debug("Will break")
+                    break
+                time.sleep(5)
+            else:
+                flag=0
+
+
+            iterx=iterx+1
+
+
+        # find all reviews
+        reviews=driver.find_elements_by_xpath("//*[@jsname='fk8dgd']//div[@class='d15Mdf bAhLNe']")
+
+
+        # dump all reviews to file
+        with open(outputFile+'.csv', mode='wb') as file:
+            writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            writer.writerow(["name","ratings","date","helpful vote","comment"])
+            for review in reviews:
+                try:
+                    soup=BeautifulSoup(review.get_attribute("innerHTML"),"lxml")
+                    name=soup.find(class_="X43Kjb").text
+                    ratings=soup.find('div',role='img').get('aria-label').strip("Rated ")[0]
+                    date=soup.find(class_="p2TkOb").text
+                    helpful=soup.find(class_="jUL89d y92BAb").text
+                    comment=soup.find('span',jsname='bN97Pc').text
+                    writer.writerow([name.encode('utf-8'),ratings,date,helpful,comment.encode('utf-8')])
+
+                except Exception , e:
+                    logger.error('Encountered exception: %s',str(e))
+
+        logger.warn('All reviews dumped to file %s.',outputFile)
+
+if __name__ == "__main__":
+    run()
+
